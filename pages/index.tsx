d8eb2d1bdb2c35ace7500db020d11ad3603a089c
szatolokinweb <szatolokin.web@gmail.com>
2022-08-18 20:22:11 +0300

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,31 +2,57 @@ import { NextPage, GetServerSideProps } from "next";
 import { loadGames } from "../api";
 import Link from "next/link";
 import { Wrapper } from "../components/Wrapper";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-let count = 1;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  console.log(`**getServerSideProps ${count++}**`);
-
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   return {
     props: {
       games: await loadGames(),
+      initialSearch: query?.searchp || "",
     },
   };
 };
 
-const Home: NextPage<{ games: Api.Game[] }> = ({ games }) => (
-  <Wrapper>
-    <ul>
-      {games.map(({ slug, name }) => (
-        <li key={slug}>
-          <Link href={`/game/${slug}`}>
-            <a>{name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Wrapper>
-);
+const Home: NextPage<{
+  games: Api.Game[];
+  initialSearch: string;
+}> = ({ games, initialSearch }) => {
+  const router = useRouter();
+
+  const [search, setSearch] = useState<string>(initialSearch);
+
+  useEffect(() => {
+    const routerQuery = { ...router.query };
+
+    if (search.trim()) {
+      routerQuery.search = search;
+    } else {
+      delete routerQuery.search;
+    }
+
+    router.push({
+      query: routerQuery,
+    });
+  }, [search]);
+
+  return (
+    <Wrapper>
+      <input
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <ul>
+        {games.map(({ slug, name }) => (
+          <li key={slug}>
+            <Link href={`/game/${slug}`}>
+              <a>{name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Wrapper>
+  );
+};
 
 export default Home;
