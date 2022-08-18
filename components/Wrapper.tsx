d18e58986c88f7a50eef657090be0c384f03a5b0
szatolokinweb <szatolokin.web@gmail.com>
2022-08-18 18:54:34 +0300

    Add Wrapper component

---
+++ components/Wrapper.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  padding: 10px;
+  max-width: 720px;
+  min-height: 100vh;
+
+  background-color: white;
+`;
