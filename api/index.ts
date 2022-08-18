7c403e8ee937136e64f77a1185a44545832ce23d
szatolokinweb <szatolokin.web@gmail.com>
2022-08-18 18:54:51 +0300

    Update api

--- api/index.ts
+++ api/index.ts
@@ -11,7 +11,7 @@ const api = axios.create({
 export const loadGames = async () =>
   (await api.get<Api.Response<Api.Game[]>>("games")).data.results;
 
-export const loadGame = async (slug: string) =>
+export const loadGameDetail = async (slug: string) =>
   await (
     await api.get<Api.GameDetail>(`games/${slug}`)
   ).data;
