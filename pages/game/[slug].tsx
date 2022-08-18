493ee27c388ff615ae5c05eac20f9a09d6f29f75
szatolokinweb <szatolokin.web@gmail.com>
2022-08-18 19:01:31 +0300

    Update game page

--- pages/game/[slug].tsx
+++ pages/game/[slug].tsx
@@ -1,7 +1,35 @@
-import { NextPage } from "next";
+import { NextPage, GetServerSideProps } from "next";
+import { loadGameDetail } from "../../api";
+import { Wrapper } from "../../components/Wrapper";
 
-const Game: NextPage = () => {
-  return <h1>hello, world</h1>;
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { slug } = params as { slug: string };
+
+  return {
+    props: {
+      gameDetail: await loadGameDetail(slug),
+    },
+  };
+};
+
+const Game: NextPage<{ gameDetail: Api.GameDetail }> = ({ gameDetail }) => {
+  const { name, released, rating, description, website } = gameDetail;
+
+  return (
+    <Wrapper>
+      <h1>{name}</h1>
+      <div>Release date: {released}</div>
+      <div>Rating: {rating}</div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: description,
+        }}
+      />
+      <a href={website} target="_blank" rel="noopener noreferrer">
+        Перейти на сайт
+      </a>
+    </Wrapper>
+  );
 };
 
 export default Game;
